ab585aa9bc9549cfc16aaa55abbda3f6d44442be
Bjørge Næss <bjoerge@gmail.com>
2020-01-27 16:35:15 +0100

    [react-hooks] Add @sanity/react-hooks package

---
+++ packages/@sanity/react-hooks/src/useValidationStatus.ts
@@ -0,0 +1,30 @@
+import documentStore from 'part:@sanity/base/datastore/document'
+import {toObservable, useObservable} from './utils/use-observable'
+import {switchMap} from 'rxjs/operators'
+import {Observable} from 'rxjs'
+
+interface Marker {
+  level: string
+  type: string
+}
+
+interface ValidationStatus {
+  isValidating: boolean
+  markers: Marker[]
+}
+
+const INITIAL: ValidationStatus = {markers: [], isValidating: false}
+
+export function useValidationStatus(publishedId, typeName): ValidationStatus {
+  return useObservable(
+    toObservable({publishedId, typeName}, props$ =>
+      props$.pipe(
+        switchMap(
+          ({publishedId, typeName}): Observable<ValidationStatus> =>
+            documentStore.local.validationFor(publishedId, typeName)
+        )
+      )
+    ),
+    INITIAL
+  )
+}
