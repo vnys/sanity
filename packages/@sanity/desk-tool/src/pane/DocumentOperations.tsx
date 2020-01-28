2c1010a0c4f094b4d871a505bbb4b9730ed986cc
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-01-28 16:02:18 +0100

    [desk-tool] Fix ts errors

--- packages/@sanity/desk-tool/src/pane/DocumentOperations.tsx
+++ packages/@sanity/desk-tool/src/pane/DocumentOperations.tsx
@@ -1,6 +1,3 @@
-import * as React from 'react'
-import {useDocumentOperationResult} from '@sanity/react-hooks'
-
 export function DocumentOperationResults(props) {
   // const event = useDocumentOperationResult(props.id, props.type)
   return null
